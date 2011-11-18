fb3748dbc7f264883a27b68964dc1e46bc6dc084
Ferllings <cedric@domogik.org>
2011-11-18 17:59:14 +0100

    Added ':' in possible address chars

--- src/domoweb/rinor/resources.py
+++ src/domoweb/rinor/resources.py
@@ -503,7 +503,7 @@ class CommandResource(RinorResource):
 
     def base_urls(self):
         return [
-            url(r"^(?P<resource_name>%s)/(?P<member>[\w\d_-]+)/(?P<address>[\w\d_-]+)%s$" % (self._meta.resource_name, trailing_slash()), self.wrap_view('dispatch_detail'), name="api_dispatch_detail"),
+            url(r"^(?P<resource_name>%s)/(?P<member>[\w\d_-:]+)/(?P<address>[\w\d_-:]+)%s$" % (self._meta.resource_name, trailing_slash()), self.wrap_view('dispatch_detail'), name="api_dispatch_detail"),
         ]
 
     def obj_update(self, bundle, request=None, **kwargs):
