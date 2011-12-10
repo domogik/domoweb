cc659e5705df9f38ef2fa2b45ea27d1a4fd70df6
Ferllings <cedric@domogik.org>
2011-12-10 23:31:41 +0100

    we

--- src/domoweb/context_processors.py
+++ src/domoweb/context_processors.py
@@ -1,9 +1,13 @@
 def rinor(request):
     from django.conf import settings
+    import datetime
+    now = datetime.datetime.now()
+
     additions = {
         'version': settings.DOMOWEB_VERSION,
         'rinor_min_version': settings.RINOR_MIN_API,
         'rinor_max_version': settings.RINOR_MAX_API,
         'dmg_min_version': settings.DMG_MIN_VERSION,
+        'we': (now.month==12 and now.day>=20 and now.day<=25),
     }
     return additions
