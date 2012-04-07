fb26c0a303adc540b011d6c99ab86242a2798dd2
vfritschy <devnull@localhost>
2012-04-07 13:30:58 +0200

    Basic Actuator No state On modification

--- src/domoweb/widgets/dmg_2x1_basicNoStateActuatorOn/main.js
+++ src/domoweb/widgets/dmg_2x1_basicNoStateActuatorOn/main.js
@@ -39,7 +39,7 @@
         action: function(command_code) {
             var self = this, o = this.options;
             //rinor.put(['api', 'command', o.devicetechnology, o.deviceaddress], {"command":command_code})
-            rinor.put(['api', 'command', o.devicetechnology, o.deviceaddress], {"command":o.model_parameters.command, "value":o.model_parameters.value1}
+            rinor.put(['api', 'command', o.devicetechnology, o.deviceaddress], {"command":o.model_parameters.command, "value":o.model_parameters.value1})
                 .done(function(data, status, xhr){
                     self.valid(o.featureconfirmation);
                 })
