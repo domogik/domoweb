377be63d3f717574cf7c654ee893f5590e434f1b
Ferllings <cedric@domogik.org>
2012-06-02 17:08:53 +0200

    Fixed plugin list (close #1345)

--- src/domoweb/design/admin/js/page-admin.js
+++ src/domoweb/design/admin/js/page-admin.js
@@ -40,44 +40,45 @@ function getPluginsList() {
     rinor.get(['api', 'plugin'])
         .done(function(data, status, xhr){
             if (data.objects && data.objects.length > 0) { // If a least 1 host exist
-                if (data.objects[0].list && data.objects[0].list.length > 0) { // If a least 1 plugin is enabled
-                    $.each(data.objects, function() {
-                        var host = this.host;
-                        $.each(this.list, function() {
-                            var technology = this.technology.replace(' ', '');
-                            if ($("#plugins_list ul#menu_" + technology).length == 0) {
-                                $("#plugins_list").append("<li><div class='titlenav2 icon16-technology-" + technology + "'>" + technology + "</div><ul id='menu_" + technology + "'></ul></li>")
-                            }
-                            var li = $("<li class='" + this.type + "'></li>");
-                            var a = $("<a></a>");
-                            a.attr('href', '/admin/plugin/' + host + "/" + this.id + "/" + this.type);
-                            var status = $("<div><div class='host'>" + host + "</div>" + this.id + "</div>");
-                            status.addClass("menu-indicator")
-                            if (this.id != 'rest') {
-                                if (this.status == 'ON') {
-                                    if (this.type == 'plugin') {
-                                        status.addClass("icon16-status-plugin-up");
-                                        status.append("<span class='offscreen'>Plugin Running</span>");                                    
-                                    } else { // external
-                                        status.addClass("icon16-status-external-up");
-                                        status.append("<span class='offscreen'>External member Running</span>");                                                                        
-                                    }
-                                } else {
-                                    if (this.type == 'plugin') {
-                                        status.addClass("icon16-status-plugin-down");
-                                        status.append("<span class='offscreen'>Plugin Stopped</span>");
-                                    } else { // external
-                                        status.addClass("icon16-status-external-down");
-                                        status.append("<span class='offscreen'>External member Stopped</span>");
-                                    }
+                var plugin_count = 0;
+                $.each(data.objects, function() {
+                    var host = this.host;
+                    $.each(this.list, function() {
+                        plugin_count++;
+                        var technology = this.technology.replace(' ', '');
+                        if ($("#plugins_list ul#menu_" + technology).length == 0) {
+                            $("#plugins_list").append("<li><div class='titlenav2 icon16-technology-" + technology + "'>" + technology + "</div><ul id='menu_" + technology + "'></ul></li>")
+                        }
+                        var li = $("<li class='" + this.type + "'></li>");
+                        var a = $("<a></a>");
+                        a.attr('href', '/admin/plugin/' + host + "/" + this.id + "/" + this.type);
+                        var status = $("<div><div class='host'>" + host + "</div>" + this.id + "</div>");
+                        status.addClass("menu-indicator")
+                        if (this.id != 'rest') {
+                            if (this.status == 'ON') {
+                                if (this.type == 'plugin') {
+                                    status.addClass("icon16-status-plugin-up");
+                                    status.append("<span class='offscreen'>Plugin Running</span>");                                    
+                                } else { // external
+                                    status.addClass("icon16-status-external-up");
+                                    status.append("<span class='offscreen'>External member Running</span>");                                                                        
+                                }
+                            } else {
+                                if (this.type == 'plugin') {
+                                    status.addClass("icon16-status-plugin-down");
+                                    status.append("<span class='offscreen'>Plugin Stopped</span>");
+                                } else { // external
+                                    status.addClass("icon16-status-external-down");
+                                    status.append("<span class='offscreen'>External member Stopped</span>");
                                 }
                             }
-                            a.append(status);
-                            li.append(a);
-                            $("#plugins_list ul#menu_" + technology).append(li);	
-                        });
+                        }
+                        a.append(status);
+                        li.append(a);
+                        $("#plugins_list ul#menu_" + technology).append(li);	
                     });
-                } else {
+                });
+                if (plugin_count == 0) { // If no plugin detected
                     var li = $("<li></li>");
                     var a = $("<a>No plugin enabled or installed<br />Click to reload</a>");
                     a.attr('href', '#');
