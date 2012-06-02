065da931dd1861e719eb7faa8f1f2e6c0eff4a8b
Ferllings <cedric@domogik.org>
2012-06-02 20:03:47 +0200

    Upgrade Django 1.4

--- src/domoweb/settings_db.py
+++ src/domoweb/settings_db.py
@@ -34,95 +34,15 @@ Implements
 @organization: Domogik
 """
 
-import os
-import commands
-import pwd
-
 DEBUG = True
-TEMPLATE_DEBUG = DEBUG
 
-ADMINS = ()
-
-MANAGERS = ADMINS
 
 LIB_PATH = '/var/lib/domoweb'
 
 ### UI Database settings
-DATABASE_ENGINE = 'sqlite3'
-DATABASE_NAME = "%s/domoweb.db" % LIB_PATH
-
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
-TIME_ZONE = 'Europe/Paris'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
-
-SITE_ID = 1
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
-USE_I18N = True
-
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = 'i#=g$uo$$qn&0qtz!sbimt%#d+lb!stt#12hr@%vp-u)yw3s+b'
-
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
-#     'django.template.loaders.eggs.load_template_source',
-)
-
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    # TODO : uncomment this once multi-languages will be supported
-    # 'django.middleware.locale.LocaleMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-)
-
-ROOT_URLCONF = 'domoweb.urls'
-
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.core.context_processors.auth',
-    'django.core.context_processors.debug',
-    'django.core.context_processors.i18n',
-    'django.core.context_processors.request',
-)
-
-INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.admin',
-    'tastypie',
-    'domoweb',
-    'domoweb.view',
-    'domoweb.admin',
-    'domoweb.rinor',
-)
-
-# Session stuff
-# Other options are :
-### 'django.contrib.sessions.backends.db'
-### 'django.contrib.sessions.backends.file'
-SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
-
-PIPES_CACHE_EXPIRY=0
-PIPES_SOCKET_TIMEOUT=600 # 600 sec
-
-#Tastypie
-API_LIMIT_PER_PAGE = 0
-
-# For login Auth
-LOGIN_URL = '/admin/login'
-LOGOUT_URL = '/admin/logout'
-LOGIN_REDIRECT_URL = '/admin/'
-
-TEMPLATE_DIRS = ()
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': "%s/domoweb.db" % LIB_PATH,
+    }
+}
