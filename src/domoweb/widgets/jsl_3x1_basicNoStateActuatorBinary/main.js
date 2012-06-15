9c8c301d53e95a94266d50a7018ba4ef722e61a7
jesuislibre <devnull@localhost>
2012-06-15 08:21:57 +0200

    Colorisation des boutons

---
+++ src/domoweb/widgets/jsl_3x1_basicNoStateActuatorBinary/main.js
@@ -0,0 +1,123 @@
+(function($) {
+    $.create_widget({
+        // default options
+        options: {
+            version: 0.1,
+            creator: 'Domogik',
+            id: 'jsl_3x1_basicNoStateActuatorBinary',
+            name: 'JSL Stateless basic widget',
+            description: 'JSL Basic switch widget (x10, broken plcbus, . . .)',
+            type: 'actuator.binary',
+            height: 1,
+            width: 3,
+            displayname: true,
+            displayborder: true
+        },
+
+        _init: function() {
+            var self = this, o = this.options;
+            this.element.addClass("icon32-usage-jsl-" + o.usage)
+              .processing();
+            // Building widget content
+            var main = $("<div class='main'></div>");
+            var on_action = $('<div class="command on">ON</div>');
+            on_action.click(function (e) {self.action(o.model_parameters.value1);e.stopPropagation();})
+                .keypress(function (e) {if (e.which == 33 || e.which == 38) {self.action(o.model_parameters.value1); e.stopPropagation();}});
+            main.append(on_action);
+            var off_action = $('<div class="command off">OFF</div>');
+            off_action.click(function (e) {self.action(o.model_parameters.value0);e.stopPropagation();})
+                .keypress(function (e) {if (e.which == 34 || e.which == 40) {self.action(o.model_parameters.value0); e.stopPropagation();}});
+            main.append(off_action);
+            
+            this.element.append(main);
+            
+            this.values = [o.model_parameters.value0, o.model_parameters.value1];
+            this.texts = [o.usage_parameters.state0, o.usage_parameters.state1];
+            this._initValues(1);
+        },
+        
+        _statsHandler: function(stats) {
+            if (stats && stats.length > 0) {
+                this.setValue(stats[0].value);
+            } else {
+                this.setValue(null);
+            }
+        },
+        
+        _eventHandler: function(timestamp, value) {
+            this.setValue(value);
+        },
+        
+        
+        action: function(command_code) {
+            var self = this, o = this.options;
+            this.element.startProcessingState();
+            if (this.currentValue) {
+                this.processingValue = (this.currentValue == 0)?1:0;                
+            } else { // Current state unknown
+                // Suppose the switch currently off
+                this.processingValue = 1;
+            }
+            rinor.put(['api', 'command', o.devicetechnology, o.deviceaddress], {"command":command_code})
+                .done(function(data, status, xhr){
+                    self.valid(o.featureconfirmation);
+                })
+                .fail(function(jqXHR, status, error){
+                    self.cancel();
+                    if (jqXHR.status == 400)
+                        $.notification('error', jqXHR.responseText);
+                });
+        },
+        cancel: function() {
+            var self = this, o = this.options;
+            this.element.stopProcessingState();
+            this._status.displayStatusError();
+        },
+
+        /* Valid the processing state */
+        valid: function(confirmed) {
+            var self = this, o = this.options;
+            this.processingValue = null;
+            this.element.stopProcessingState();
+            if (confirmed) {
+                this._status.displayStatusOk();
+                this.element.doTimeout( 'resetStatus', state_reset_status, function(){
+                    self._status.displayResetStatus();
+                });
+            } else {
+                self._status.displayResetStatus();                
+            }
+        },
+        
+        setValue: function(value) {
+            if (value != null) {
+                if (value == 1 || (typeof(value) == 'string' && value.toLowerCase() == this.values[1])) {
+                    this.currentValue = 1;
+                } else if (value == 0 || (typeof(value) == 'string' && value.toLowerCase() == this.values[0])) {
+                    this.currentValue = 0;
+                }                
+            } else { // Unknown
+                this.currentValue = null;
+            }
+            this.processingValue = null;
+            this.displayValue(this.currentValue);
+        },
+        
+        
+        displayValue: function(value) {
+            var self = this, o = this.options;
+            if (value != null) {
+                if (value == 1) {
+                    this.element.displayIcon('value_1');             
+                } else {
+                    this.element.displayIcon('value_0');             
+                }
+                this._status.writeStatus(this.texts[value]);
+            } else { // Unknown
+                this.element.displayIcon('unknown');                             
+                this._status.writeStatus('---');
+            }
+        }
+
+    });
+})(jQuery);
