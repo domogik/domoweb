3450a80957f41ce6c31245d003c48bc9fd11a659
Ferllings <cedric@domogik.org>
2012-07-03 18:02:40 +0200

    Updated PageIcon model

--- src/domoweb/models.py
+++ src/domoweb/models.py
@@ -10,5 +10,6 @@ class Widget(models.Model):
 class PageIcon(models.Model):
     id = models.CharField(max_length=50, primary_key=True)
     iconset_id = models.CharField(max_length=50)
+    iconset_name = models.CharField(max_length=50)
     icon_id = models.CharField(max_length=50)
     label = models.CharField(max_length=50)
