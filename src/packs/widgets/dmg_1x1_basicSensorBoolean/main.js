b4829d4b8d2cd68848f2a69e7fb16fa103a6c817
vfritschy <devnull@localhost>
2012-10-10 18:31:35 +0200

    correct dmg_1x1_basicSensorBoolean for use parameter

--- src/packs/widgets/dmg_1x1_basicSensorBoolean/main.js
+++ src/packs/widgets/dmg_1x1_basicSensorBoolean/main.js
@@ -17,6 +17,8 @@
         _init: function() {
             var self = this, o = this.options;
             this.element.addClass("icon32-usage-" + o.usage)
+            this.TRUE=o.model_parameters.value1
+            this.FALSE= o.model_parameters.value0
 
             this._status = $.getStatus();
             this.element.append(this._status);
@@ -38,12 +40,13 @@
 
         setValue: function(value, unit, previous) {
             var self = this, o = this.options;
-            if (value) {
+            if (value==this.TRUE || value==this.FALSE) {
                 value = value.toLowerCase();
-                if (value == "high") {
+                if (value == this.TRUE) {
                     this.element.displayIcon('value_true');             
                     this._status.removeClass('icon16-status-unknown icon16-status-inactive').addClass('icon16-status-active');
-                } else { // low
+                }
+                if(value== this.FALSE) { // low
                     this.element.displayIcon('value_false');             
                     this._status.removeClass('icon16-status-unknown icon16-status-active').addClass('icon16-status-inactive');
                 }
