b4d24a68487c093514d791975796f679351d8ece
Ferllings <cedric@domogik.org>
2014-07-09 21:44:56 +0200

    Added New Digital Clock Widget

---
+++ packs/widgets/time/js/digitalClock.js
@@ -0,0 +1,44 @@
+
+
+
+	// Run a timer every second and update the clock
+
+	(function update_time(){
+
+		// Use moment.js to output the current time as a string
+		// hh is for the hours in 12-hour format,
+		// mm - minutes, ss-seconds (all with leading zeroes),
+		// d is for day of week and A is for AM/PM
+
+		var now = moment().format("hhmmssdA");
+
+		digits.h1.attr('class', digit_to_name[now[0]]);
+		digits.h2.attr('class', digit_to_name[now[1]]);
+		digits.m1.attr('class', digit_to_name[now[2]]);
+		digits.m2.attr('class', digit_to_name[now[3]]);
+		digits.s1.attr('class', digit_to_name[now[4]]);
+		digits.s2.attr('class', digit_to_name[now[5]]);
+
+		// The library returns Sunday as the first day of the week.
+		// Stupid, I know. Lets shift all the days one position down, 
+		// and make Sunday last
+
+		var dow = now[6];
+		dow--;
+		
+		// Sunday!
+		if(dow < 0){
+			// Make it last
+			dow = 6;
+		}
+
+		// Mark the active day of the week
+		weekdays.removeClass('active').eq(dow).addClass('active');
+
+		// Set the am/pm text:
+		ampm.text(now[7]+now[8]);
+
+		// Schedule this function to be run again in 1 sec
+		setTimeout(update_time, 1000);
+
+	})();
